d846afa605945301b53043e007ae076df6ff0bbf
NocturneCrowz <48096588+NocturneCrowz@users.noreply.github.com>
2019-05-16 17:33:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
-Header("Custom-Header: Ciao");
-Header("Content-Type: application/json");
-
-echo json_encode(array(
-    "property" => "value",
-    "other_property" => array(
-        "subprop" => 123
-    )
-));
+echo "Ciao Mondo!";
+?>
